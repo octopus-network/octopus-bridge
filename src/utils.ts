8103834b77c0080c683ef749c209ae4b8e40713e
czl1378 <czl1378@126.com>
2021-09-04 05:35:50 +0800

    update token decimals

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 import { BigNumber } from "bignumber.js";
 
-export function fromDecimals(numStr, decimals = 24) {
+export function fromDecimals(numStr, decimals = 18) {
   return new BigNumber(numStr).div(Math.pow(10, decimals)).toNumber();
 }
 
-export function toDecimals(num, decimals = 24) {
+export function toDecimals(num, decimals = 18) {
   return new BigNumber(num).multipliedBy(10 ** (decimals)).toString(10);
 }
